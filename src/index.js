17f997997aaaf4d80092b7922acbb50989d78a7e
Yefim Vedernikoff <yefim323@gmail.com>
2018-04-02 22:51:44 -0700

    Get react involved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './styles.css';
 
-console.log('wow');
+import React from 'react';
+import {render} from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+      <h1>Dot-file</h1>
+    );
+  }
+}
+
+render(
+  <App />,
+  document.getElementById('app')
+);
