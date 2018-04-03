08627bfe75f58b90cb5036dbd0ce1b6e57f97c96
Yefim Vedernikoff <yefim323@gmail.com>
2018-04-02 22:42:41 -0700

    Use hash in js file

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
+const path = require('path');
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   plugins: [
+    new CleanWebpackPlugin(['dist']),
     new HtmlWebPackPlugin({
       template: 'src/index.html'
     }),
-    new MiniCssExtractPlugin()
+    new MiniCssExtractPlugin(),
+    new webpack.HashedModuleIdsPlugin()
   ],
   module: {
     rules: [
@@ -30,5 +35,9 @@ module.exports = {
         use: [MiniCssExtractPlugin.loader, 'css-loader']
       }
     ]
+  },
+  output: {
+    filename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, 'dist')
   }
 };
