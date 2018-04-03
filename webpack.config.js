fdb0b0865084f4305c1bf18682ab5551ea90444f
Yefim Vedernikoff <yefim323@gmail.com>
2018-04-02 22:23:49 -0700

    Add html and css support

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,30 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   plugins: [
+    new HtmlWebPackPlugin({
+      template: 'src/index.html'
+    }),
     new MiniCssExtractPlugin()
   ],
   module: {
     rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {minimize: true}
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
       {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader']
