9f0398d4b44861c84ad6ad96a70cbe4a097bde55
Yefim Vedernikoff <yefim323@gmail.com>
2018-04-02 17:32:36 -0700

    Add CSS support

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  plugins: [
+    new MiniCssExtractPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      }
+    ]
+  }
+};
