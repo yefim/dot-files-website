ee72e21878e1ebf60d6176e15007365e527dec61
Yefim Vedernikoff <yefim323@gmail.com>
2018-04-07 17:32:37 -0700

    Add more to itempage

--- src/ItemPage.js
+++ src/ItemPage.js
@@ -24,7 +24,68 @@ export default class ItemPage extends React.Component {
         <h1>{username}&apos;s dotfiles</h1>
         <div>
           <h2><a href="#link-to-vimrc">.vimrc</a></h2>
-          <pre>
+          <pre style={{'max-height': '400px', overflow: 'scroll'}}>
+{`" Use the Solarized Dark theme
+set background=dark
+colorscheme solarized
+let g:solarized_termtrans=1
+
+" Make Vim more useful
+set nocompatible
+" Use the OS clipboard by default
+set clipboard=unnamed
+" Enhance command-line completion
+set wildmenu
+" Allow cursor keys in insert mode
+set esckeys
+" Allow backspace in insert mode
+set backspace=indent,eol,start
+" Optimize for fast terminal connections
+set ttyfast
+" Add the g flag to search/replace by default
+set gdefault
+" Use UTF-8 without BOM
+set encoding=utf-8 nobomb
+" Change mapleader
+let mapleader=","
+" Don’t add empty newlines at the end of files
+set binary
+set noeol
+" Centralize backups, swapfiles and undo history
+set backupdir=~/.vim/backups`}
+          </pre>
+        </div>
+        <div>
+          <h2><a href="#link-to-vimrc">.bashrc</a></h2>
+          <pre style={{'max-height': '400px', overflow: 'scroll'}}>
+{`" Use the Solarized Dark theme
+set background=dark
+colorscheme solarized
+let g:solarized_termtrans=1
+
+" Make Vim more useful
+set nocompatible
+" Use the OS clipboard by default
+set clipboard=unnamed
+" Enhance command-line completion
+set wildmenu
+" Allow cursor keys in insert mode
+set esckeys
+" Allow backspace in insert mode
+set backspace=indent,eol,start
+" Optimize for fast terminal connections
+set ttyfast
+" Add the g flag to search/replace by default
+set gdefault
+" Use UTF-8 without BOM
+set encoding=utf-8 nobomb
+" Change mapleader
+let mapleader=","
+" Don’t add empty newlines at the end of files
+set binary
+set noeol
+" Centralize backups, swapfiles and undo history
+set backupdir=~/.vim/backups`}
           </pre>
         </div>
       </div>
